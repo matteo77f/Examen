f981a5955dc4d1afe50e27577245d6f015f53534
matteo77f <64443838+matteo77f@users.noreply.github.com>
2020-06-19 19:25:40 -0300

    Add files via upload
    modificacion verificacion existe cliente

--- Script.sh
+++ Script.sh
@@ -6,20 +6,34 @@ if [[ -z $1 || $1 == "-h" || $1 == "-help" || $1 == "-?" ]]; then
 	echo "Ayuda:"
 	echo "Primer parametro: seller_id ."
 	echo "Segundo parametro: site_id ."
+	echo "Recordar que tiene que tener permisos de escritura el directorio donde ejecuta este script ya que en el mismo se crea un archivo Log.txt"
+	
  	exit
 fi
 #!----------------------------------------------
+#! verifico si existe el archivo log, de existir lo elimino.
+arch = Log
+if [ -f $arch ]
+then
+   rm $arch
+fi
 
 #!busco los articulos y datos de clientes con la api de MeLi y los guardo en una archivo de respaldo
 datos_seller = curl -X GET https://api.mercadolibre.com/sites/$2/search?seller_id=$1 ;
 
-#! extraigo del archivo aux el numero de producto y lo guardo en un vector
-vec_pro = fgrep -i  \"id\"\: \ \"$2 <<<< datos_seller;
+#!{"message":"Invalid user id 02593498","error":"bad_request","status":400,"cause":[]}
+
+#!verificamos si existe el cliente o retorna error.
+existe =fgrep -i  \"status\"\: <<<< datos_seller;
+existe= ${existe:9} ;
+if ( $existe != 400) 
+	#! extraigo del archivo aux el numero de producto y lo guardo en un vector
+	vec_pro = fgrep -i  \"id\"\: \ \"$2 <<<< datos_seller;
 
     for i in $vec_pro
     do
 	#! corto los tag de los numero de producto
-	vec_pro[i]= ${vec_pro[i]:7:-1} ; 
+	v 
 	
 	#!con los datos del vector busco con la api de MeLi saco los datos del producto y el numero de la categotia
 	datos_producto = curl -X GET https://api.mercadolibre.com/items/$vec_pro[i];
@@ -46,9 +60,9 @@ vec_pro = fgrep -i  \"id\"\: \ \"$2 <<<< datos_seller;
 	vec_cat[i]= ${vec_cat[i]:8:-1} ; 
 	
 		
-	echo $vec_pro[i] del ítem, vec_titulo[i] del item, vec_cat[i] donde está publicado, vec_cat_nom[i] de la categoría. >> log.txt
+	echo " '$vec_pro[i]' del ítem, '$vec_titulo[i]' del item, '$vec_cat[i'] donde está publicado, '$vec_cat_nom[i]' de la categoría." >> Logog.txt
 	
 	done
+fi
 
-
-
+echo "El cliente pasado por parametro no existe para el site informado" >> Logog.txt
